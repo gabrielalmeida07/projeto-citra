ca9a20aa6dda517915a57e57fbf39bf8f6cc69b8
gabrielalmeida07 <ga.fernandes07@gmail.com>
2018-06-07 19:06:58 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# projeto-citra
+# [Lading Page - Citra]()
+
+
+## Estrutura tecnológica da aplicação:
+
+Front-End:  biblioteca Bootstrap e Template do Bootstrap Free Themes, com HTML5, CSS3, Javascript, JSON, jQuerry, Automatizadores Gulp, e muito mais.
+
+Back–end desenvolvido na linguagem multi-paradigma PHP. 
+
+Utilização de SQL, em especial as subcategorias DDL e DML, com o Sistema Gerenciador de Banco de Dados MySQL. Utilização da interface PHPMyAdmin para manipulação do banco de dados. 
+
+Hospedagem local utilizando servidor do tipo APACHE através da aplicação XAMPP. 
+
+## Estrutura de conteúdo:
+
+Desenvolvimento utilizando técnicas de SEO para produção de conteúdo WEB como: Psicologia de Busca;  Estruturação de palavras-chave; Fatores de Rankeamento; Call to Action; Link Building e Marketing de conteúdo para WEB.
+Pauta baseada em:
+
+•	Título da pauta:
+O que esperar da tecnologia no futuro?
+
+
+•Palavra-chave: O que esperar da tecnologia;
+
+•Persona: Pedro, 42 anos, gestor da área de tecnologia. Um dos seus principais desafios é ficar de olho nas inovações tecnológicas que acontecem no exterior;
+
+•	Tipo de CTA:
+Assinar newsletter;
+
+•	Estágio do funil:
+Atração;
+
+•	Empresa:
+Citra.
+
+## Copyright and License
+
+Copyright 2013-2018 Blackrock Digital LLC. Code released under the [MIT](https://github.com/BlackrockDigital/startbootstrap-creative/blob/gh-pages/LICENSE) license.
+
+
---
+++ index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Citra</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <style type="text/css">
+      
+      
+      #logo{width:20%;height: 10%;}
+      .btn-secondy{border-radius:13px;background-color:#dfdfdf;border-color:#ffffff;color:black;}
+      .btn-secondy:hover{color:#ffffff;}.btn-secondy:focus{color:#ffffff;}
+      .section-heading.text-white{margin-top:-10%;}
+      .section{background-color: black;}
+      .text-uppercase{margin-top:70px;}
+      
+      
+    </style>
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a href="http://www.grupocitra.com.br/" class="navbar-brand js-scroll-trigger" href="#page-top"><img id="logo" src="./img/citra.png"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">Sobre</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services">Conteúdo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio">Tecnologias</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contato</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <header class="masthead text-center text-white d-flex">
+      <div class="container my-auto">
+        <div class="row">
+          <div class="col-lg-10 mx-auto">
+            <h1 class="text-uppercase">
+              <a style="color:#1c9de7e5" ><img src="https://fontmeme.com/permalink/180605/6af9c6fa3d9c4f11f3c3c33d286bdb7b.png" alt="fonte-planet-n-compact" border="0"width="80%" ></a> 
+            
+            </h1>
+            
+          </div>
+          <div class="col-lg-8 mx-auto">
+            <p class="text-faded mb-5"> </p>
+            <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Continue lendo..</a>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading text-white">Imaginar como será o futuro parece algo fácil? Sabemos que na realidade não é bem assim! Quase todos os dias são lançadas novas tecnologias, nos deixando um pouco perdidos com tantas novidades. Separamos aqui, algumas tecnologias emergentes que mudarão completamente a maneira que vivemos. Afinal, o que esperar da tecnologia? Continue lendo nosso post e veja o que acontecerá nos próximos anos..</h2>
+            <hr class="light my-4">
+            <a class="btn btn-secondy btn-xl js-scroll-trigger" href="#services">Vamos lá!</a>
+ 
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Como são feitas as previsões tecnológicas?</h2>
+            <hr class="my-4">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 ">
+        <p>Quem nunca ouviu falar de Nostradamus e suas profecias? Entre suas profecias mais famosas, estão o atendado de 11 de setembro e a ascensão de Adolf Hitler ao poder na Alemanha. Nostradamus foi um homem completamente fora de seu tempo, apesar de ter vivido nos anos de 1500, suas profecias ainda são conhecidas pelo mundo todo e deixam as pessoas intrigadas até os dias de hoje.</p>
+        
+        <p>Diferente das profecias de Nostradamus, a previsão tecnológica, com exceção de novas tecnologias, é realizada muitas vezes por projeções de tecnologias já existentes.</p>
+
+        <p>Dessa maneira, podemos analisar tecnologias com projeções para um futuro próximo, que terão acesso para a maioria de nós e mudarão drasticamente nossos costumes e estilos de vida. Startups vêm ganhando força com ideias disruptivas e pessoas como Bill Gates já até investiram em tecnologias emergentes, como na Menhphis Meat, que produz carne de laboratório. Veja outras:</p>
+        <ul>
+          <li>Cidades subaquáticas<a href="https://www.youtube.com/watch?v=uFG89OiFIbI">(Shimizu Corp)</a></li>
+          <li>Drones para transporte pessoal<a href="http://www.ehang.com/ehang184">(EHANG 184)</a></li>
+          <li>Sua casa feita em 24 horas por uma impressora 3D<a href="http://apis-cor.com/en/">(APIS COR)</a></li>
+          <li>Aeronaves movidas a Laser<a href="https://www.youtube.com/watch?v=XhUasBcoj-Q">(Lightcraft)</a></li>
+          <li>Imagine matar a saudade de alguém, podendo tocá-la(o) sem sair de casa?<a href="http://www.hapis.k.u-tokyo.ac.jp/?portfolio=haptoclone&lang=en">(Haptoclone)</a></li>
+          <li>Primeiros passos para a vida eterna<a href="https://www.popsci.com/humai-wants-to-resurrect-humans-within-30-years">(HUMAI)</a></li>
+          <li>E<a href="https://epocanegocios.globo.com/Tecnologia/noticia/2018/01/30-startups-para-voce-ficar-de-olho-em-2018.html"> muito mais</a>;</li>
+        </ul>
+      </div>
+      </div>
+      </div>
+      </div>
+    </section>
+
+    <section class="p-0" id="portfolio">
+      <div class="container-fluid p-0">
+        <div class="row no-gutters popup-gallery">
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Cidades subaquáticas
+                  </div>
+                  <div class="project-name">
+                    Shimizu Corp
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Taxi Drone
+                  </div>
+                  <div class="project-name">
+                    EHANG 184
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Printing buildings 
+                  </div>
+                  <div class="project-name">
+                    APIS COR
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/4.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Aeronaves movidas a Laser
+                  </div>
+                  <div class="project-name">
+                    Light Craft
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/5.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/5.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Holograma de Telepresença
+                  </div>
+                  <div class="project-name">
+                    Haptoclone
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div class="col-lg-4 col-sm-6">
+            <a class="portfolio-box" href="img/portfolio/fullsize/6.jpg">
+              <img class="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="">
+              <div class="portfolio-box-caption">
+                <div class="portfolio-box-caption-content">
+                  <div class="project-category text-faded">
+                    Eternal life
+                  </div>
+                  <div class="project-name">
+                    HUMAI
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </br>
+  <section id="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12 ">
+      <h2 class="text-center">Por que inovar em seus negócios?</h2> 
+      <hr class="my-4">
+        <p>Para continuar vivo e manter a competitividade é preciso se reinventar a cada dia!</p>
+        <p>Quem imaginaria que empresas como a Kodak ou Blockbuster iriam por água abaixo depois de dominarem o mercado em seus respectivos segmentos? Ambas insistiram em não acompanhar o avanço tecnológico e sentiram na pele seu próprio fracasso.</p>
+        <p>A saída para esses problemas podem ser duas soluções: realizar parcerias com Startups ou criar programas de inovações<a href="http://www.dictech.com.br/dicionario/termos-tecnicos/aviacao/significado-de/r-d/"(R&D)></a> internos.</p>
+        <p>A inovação precisa estar presente em todas as empresas. Seja ela grande, média ou de pequeno porte. Junto com a inovação vêm o risco, e talvez seja por isso que grandes empresas ainda insistem em permanecer como estão. Startups teoricamente têm menos a perder pelo seu porte pequeno, e por isso vêm dominando o mercado em diversos setores com negócios disruptivos.</p>
+        <p>A inovação precisa ser uma cultura enraizada na empresa, algo que aconteça repentinamente. Sua empresa precisa favorecer um ambiente que desperte e estimule a criatividade dos seus colaboradores, fazendo com que se sintam motivados a entregar resultados e a continuarem com a empresa.</p> </br></br>
+        <h2 class="text-center">O que esperar da tecnologia para os próximos anos?</h2>
+        <hr class="my-4">
+        <p>A maioria das empresas veem o melhor profissional hoje sendo um especialista com muita experiência. Entretanto, as empresas que terão sucesso no futuro, são aquelas que procuram profissionais com muita ambição, interesse e dedicação em aprender coisas novas, pessoas que se adaptam facilmente, estão sempre atualizadas e não têm medo de mudanças.</p>
+        <p>O profissional ideal do século XX era aquele com um ótimo conhecimento técnico sobre a tarefa que realizava. Esse mesmo profissional se mostrou ineficiente quando o assunto são mudanças. A comodidade deu espaço ao desespero!</p>
+        <p>Já deve estar cansado de escutar que o número de pessoas desempregadas só tende a aumentar no futuro, e que a maioria das profissões de amanhã já nem existem. Mudanças extraordinárias acontecerão no mercado de trabalho, e apenas profissionais extraordinários sobreviverão.</p>
+        <p>Seu trabalho dependerá de sua capacidade em aprender coisas novas rapidamente, ter conhecimento interdisciplinar e saber resolver problemas. Principalmente pela evolução da inteligência artificial, muitas tarefas manuais se tornarão obsoletas para o trabalho humano e facilmente substituídas por um software de IA.</p>
+        <p>Na era da informação só é preciso ter força de vontade e determinação para se atualizar. Um curso que antes custava milhares, hoje em dia custa centenas, quando não for de graça! Se atualizar agora é mais fácil e mais barato!</p>
+        <p>Por se tratar de previsões, não podemos afirmar com toda a certeza que tudo isso realmente acontecerá, mas grande parte das mudanças já começam a ser vistas hoje.</p>
+       
+    </div>
+    </div>
+    </div>
+    </div>
+  </section>
+    <section class="bg-dark text-white">
+      <div class="container text-center">
+        <h2 class="mb-4">Quer saber mais sobre o futuro?</h2>
+
+       <!-- registrando email do usuario -->
+
+        <form method="post" action="registra_usuario.php">
+          <div>
+           
+            <div class="col-auto">
+              <label class="sr-only" for="inlineFormInputGroup">Digite seu email</label>
+              <div class="input-group mb-2">
+                <div class="input-group-prepend">
+                  <div class="input-group-text"><button type="submit" class="btn btn-secondy ">Enviar</button></div>
+                </div>
+                <input type="email" name="email" class="form-control" id="inlineFormInputGroup" placeholder="Digite seu email">
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Conheça mais sobre a Citra!</h2>
+            <hr class="my-4">
+            <p class="mb-5"><strong>Acesse: <a href="www.grupocitra.com.br"> http://www.grupocitra.com.br/</a></strong></p>
+          </div>
+        </div>
+        
+      </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+
+     <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120556601-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-120556601-1');
+</script>
+
+  </body>
+
+</html>
